1b87c15daa5f8565855e21449abc638c50a83e74
Ståle <staale_80@hotmail.com>
2024-03-05 13:52:05 +0100

    Buildt buttons and styled Welcomescreen + div tutorial

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
   return <WelcomeScreen />;
