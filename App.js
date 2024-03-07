79a8f0b8b755369a8915cea741f0ef8aedeffdf9
Ståle <staale_80@hotmail.com>
2024-03-07 12:24:15 +0100

    Built MessagesScreen and ListItemSeparator

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
