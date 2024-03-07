6a661576a134f7fbad577b99fe830f69c0dd6048
Ståle <staale_80@hotmail.com>
2024-03-07 12:52:24 +0100

    Added Swiping

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,25 +1,27 @@
-import React from "react";
-import {
-  View,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  TouchableHighlight,
-} from "react-native";
-import AppText from "./AppText";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { Image, StyleSheet, TouchableHighlight, View } from "react-native";
 import colors from "../config/colors";
+import AppText from "./AppText";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
 
-function ListItem({ title, subtitle, image, onPress }) {
+function ListItem({ title, subtitle, image, onPress, renderRightActions }) {
   return (
-    <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
-      <View style={styles.container}>
-        <Image style={styles.image} source={image} />
-        <View>
-          <AppText style={styles.title}>{title}</AppText>
-          <AppText style={styles.subtitle}>{subtitle}</AppText>
-        </View>
-      </View>
-    </TouchableHighlight>
+    <GestureHandlerRootView>
+      <Swipeable renderRightActions={renderRightActions}>
+        <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+          <View style={styles.container}>
+            <Image style={styles.image} source={image} />
+            <View>
+              <AppText style={styles.title}>{title}</AppText>
+              <AppText style={styles.subtitle}>{subtitle}</AppText>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 }
 
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -3,6 +3,7 @@ import { FlatList, View } from "react-native";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
 import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
 
 const messages = [
   {
@@ -31,6 +32,7 @@ function MessagesScreen(props) {
             subtitle={item.description}
             image={item.image}
             onPress={() => console.log("pressed", item)}
+            renderRightActions={ListItemDeleteAction}
           />
         )}
         ItemSeparatorComponent={() => <ListItemSeparator />}
