872221597470699ad7d29400efd8cb5e0f6ed272
Ståle <staale_80@hotmail.com>
2024-03-12 12:44:01 +0100

    Created custom Function component snippet

---
+++ app/components/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+function Test(props) {
+  return <View style={styles.contianer}></View>;
+}
+
+const styles = StyleSheet.create({
+  contianer: {},
+});
+
+export default Test;
